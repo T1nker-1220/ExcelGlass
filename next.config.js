9e3ddeb22847064bdd2621e0a67bc843d5b82c5f
T1nker-1220 <marquezjohnnathanieljade@gmail.com>
2024-11-28 17:41:01 +0800

    Deployment in vercel

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'], // Add other image domains as needed
   },
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 }
 
 module.exports = nextConfig
